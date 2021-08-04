9837f759cdb16349c9d22d110ffba56baa642f5d
Felipe Moura <felipe.moura@interprocess.com.br>
2021-08-04 20:48:43 -0300

    criação do projeto

---
+++ ByteBank/Exception/OperacaoFinanceiraException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class OperacaoFinanceiraException : Exception
+    {
+        public OperacaoFinanceiraException() { }
+        public OperacaoFinanceiraException(string menssagem) { }
+
+        public OperacaoFinanceiraException(string menssagem, Exception excecaoInterna): base(menssagem, excecaoInterna ) { }
+    }
+}
